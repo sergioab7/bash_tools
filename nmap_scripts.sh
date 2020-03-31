e328084117cd49df5497c85de73570d15fa146e9
Sergio Andujar <sergioandujar@xaxxjs.local>
2020-04-01 01:01:04 +0200

    test

--- nmap_scripts.sh
+++ nmap_scripts.sh
@@ -58,25 +58,61 @@ function totalScan(){
 				read nombre_archivo 
 
 			fi
-		else 
-			echo -e "Creando un nuevo archivo llamado: $nombre_archivo"
-			sleep 1 
-			echo -e "Ejecutando script..."
-			sleep 1
-			nmap -sC -sV -Pn $ip_direccion >> $nombre_archivo
-			if [ -f $nombre_archivo ]; then
-				echo -e "Escaneo completado"
-			else
-				echo -e "Escaneo no completado"
-				exit 
-			fi
-		fi 
-
-	if [ "$respuesta" == "no" ]; then
-		echo -e "Guardando en otro directorio."
-		exit 1
 		fi
 	fi 
+
+	echo -e "Creando un nuevo archivo llamado: $nombre_archivo"
+	sleep 1 
+	echo -e "Ejecutando script..."
+	sleep 1
+	nmap -sC -sV -Pn $ip_direccion >> $nombre_archivo
+	if [ -f "$nombre_archivo" ]; then
+		echo -e "Escaneo no completado"
+	else
+		echo -e "Escaneo completado"
+		exit 	
+	fi 
+}
+
+function standarScan(){
+	clear
+	echo -e "${purpleColour} Standard Scan ${endColour}"
+	echo -e "IP>>"
+	read dire_IP
+	echo -e "Direccion IP establecida: $dire_IP"
+	echo -e "Deseas guardar el archivo en el directorio: (si/no)" && pwd
+	read respuesta 
+	if [ $respuesta == "si" ]; then
+	sleep 1
+	echo -e "Como se llamará el archivo? "
+	read nombre_archivo 
+		if [ -f $nombre_archivo ]; then 
+		echo -e "El archivo ya existe. Deseas sobreescribirlo? (si/no)" 
+		read respuesta_de_archivo
+			if [ "$respuesta_de_archivo" == "si" ]; then 
+				echo -e "Creando escaneo ESTANDARD y guardando con el nombre: $nombre_archivo"
+				nmap -sS $dire_IP > $nombre_archivo
+				echo -e "Escaneo completado y guardado en: $nombre_archivo"
+			fi 
+			if [ "$respuesta_de_archivo" == "no" ];then 
+				echo -e "Nuevo nombre para el archivo: "
+				echo -e "next"
+			fi
+		fi
+	fi
+
+	echo -e "Creando un nuevo archivo llamado: $nombre_archivo"
+	sleep 1
+	echo -e "Ejecutando script... "
+	sleep 1
+	nmap -sS $dire_IP > $nombre_archivo
+	if [ -f "$nombre_archivo" ]; then
+		echo -e "Escaneo no completado"
+	else
+		echo -e "Escaneo completado"
+		exit 	
+	fi 
+
 }
 
 if [ "$(id -u)" -eq "0" ]; then
@@ -88,6 +124,27 @@ if [ "$(id -u)" -eq "0" ]; then
 		
 		case $opcion_nmap in
 			1) totalScan
+			;;
+
+			2) standarScan
+			;;
+
+			3) versionesScan
+			;;
+
+			4) firewallScan
+			;;
+
+			5) UDPScan
+			;;
+
+			6) TCPScan
+			;;
+
+			7) AllScan
+			;;
+
+			8) ScriptsVuln
 			;;
 
 			*) exit
