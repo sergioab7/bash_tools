a69d3cf6bae21ef3a2100cae818a57352bdab579
Sergio Andujar <sergioandujar@xaxxjs.local>
2020-04-01 02:36:22 +0200

    Falta

--- nmap_scripts.sh
+++ nmap_scripts.sh
@@ -30,6 +30,7 @@ function menu(){
 	echo -e "9) Escaneo de scripts SMB"
 	echo -e "10) Escaneo de scripts HTTP"
 	echo -e "11) Escaneo de <SELECCIONA PUERTOS>"
+	echo -e "12) Mostrar scripts nmap (por categoria)"
 	read opcion_nmap
 }
 
@@ -54,7 +55,6 @@ function totalScan(){
 			if [ "$respuesta_archivo_existente" == "no" ]; then
 				echo -e "Dime un nuevo nombre para el archivo: "
 				read nombre_archivo 
-
 			fi
 		fi
 	fi 
@@ -113,6 +113,211 @@ function standarScan(){
 
 }
 
+function versionesScan(){
+	clear
+	echo -e "${purpleColour}Escaneo de versiones/vulnerabilidades/SO${endColour}"
+	echo -e "Direccion IP>>"
+	read dire_IP
+	echo -e "Direccion IP establecida: $dire_IP"
+	sleep 2
+
+	echo -e "Deseas guardar el archivo en el directorio: (si/no)" && pwd
+	read respuesta 
+	if [ $respuesta == "si" ]; then
+	sleep 1
+	echo -e "Como se llamará el archivo? "
+	read nombre_archivo 
+		if [ -f $nombre_archivo ]; then 
+		echo -e "El archivo ya existe. Deseas sobreescribirlo? (si/no)" 
+		read respuesta_de_archivo
+			if [ "$respuesta_de_archivo" == "si" ]; then 
+				echo -e "Creando escaneo ESTANDARD y guardando con el nombre: $nombre_archivo"
+				nmap -sS $dire_IP > $nombre_archivo
+				echo -e "Escaneo completado y guardado en: $nombre_archivo"
+			fi 
+			if [ "$respuesta_de_archivo" == "no" ];then 
+				echo -e "Nuevo nombre para el archivo: "
+				echo -e "next"
+			fi
+		fi
+	fi
+
+	echo -e "Creando un nuevo archivo llamado: $nombre_archivo"
+	sleep 1
+	echo -e "Ejecutando script... "
+	sleep 1
+	nmap -sV -sC -sO -A $dire_IP > $nombre_archivo
+
+	if [ -f "$nombre_archivo" ]; then
+		echo -e "Escaneo no completado"
+	else
+		echo -e "Escaneo completado"
+		exit 	
+	fi 
+
+	
+}
+
+function ScriptsNmap(){
+	clear
+	echo -e "${purpleColour}Mostrar todos los scripts de nmap${endColour}"
+	sleep 1
+	echo -e "1. Mostrar todos"
+	echo -e "2. Mostrar servicios ftp"
+	echo -e "3. Mostrar servicios smb"
+	echo -e "4. Mostrar servicios ssh"
+	echo -e "5. Mostrar servicios http"
+	echo -e "6. Mostrar servicios mysql"
+	echo -e "7. Buscar tu propio servicio."
+	read servicio_mostrar 
+
+	if [ $servicio_mostrar -eq "1" ]; then 
+		clear
+		echo -e "${redColour} Opcion seleccionada: TODOS ${endColour}"
+		echo -e "Ejecutando script..."
+		sleep 1
+		for i in $(locate "*.nse"); do echo "Script: " $i; done
+		sleep 4
+		exit
+	fi
+
+	if [ $servicio_mostrar -eq "2" ]; then 
+		clear
+		echo -e "${redColour} Opcion seleccionada: ftp ${endColour}"
+		echo -e "Ejecutando script..."
+		sleep 1
+		for i in $(locate "*.nse" | grep ftp); do echo "Script: " $i; done
+		sleep 4
+		exit
+	fi
+
+	if [ $servicio_mostrar -eq "3" ]; then 
+		clear
+		echo -e "${redColour} Opcion seleccionada: smb ${endColour}"
+		echo -e "Ejecutando script..."
+		sleep 1
+		for i in $(locate "*.nse" | grep smb); do echo "Script: " $i; done
+		sleep 4
+		exit
+	fi
+
+	if [ $servicio_mostrar -eq "4" ]; then 
+		clear
+		echo -e "${redColour} Opcion seleccionada: ssh ${endColour}"
+		echo -e "Ejecutando script..."
+		sleep 1
+		for i in $(locate "*.nse" | grep ssh); do echo "Script: " $i; done
+		sleep 4
+		exit
+	fi
+
+	if [ $servicio_mostrar -eq "5" ]; then 
+		clear
+		echo -e "${redColour} Opcion seleccionada: http ${endColour}"
+		echo -e "Ejecutando script..."
+		sleep 1
+		for i in $(locate "*.nse" | grep http); do echo "Script: " $i; done
+		sleep 4
+		exit
+	fi
+
+	if [ $servicio_mostrar -eq "6" ]; then 
+		clear
+		echo -e "${redColour} Opcion seleccionada: mysql ${endColour}"
+		echo -e "Ejecutando script..."
+		sleep 1
+		for i in $(locate "*.nse" | grep mysql); do echo "Script: " $i; done
+		sleep 4
+	fi
+
+	if [ $servicio_mostrar -eq "7" ]; then 
+		clear
+		echo -e "${blueColour}Selecciona el servicio que quieres: ${endColour}"
+		read mostrar_servicio_usuario
+		for i in $(locate "*.nse" | grep $mostrar_servicio_usuario); do echo "Script: " $i; done
+		sleep 2
+	fi
+}
+
+function UDPScan(){
+	clear
+	echo -e "${purpleColour} UDP Scan${endColour}"
+	echo -e "IP>>"
+	read dire_IP
+	echo -e "Direccion IP establecida: $dire_IP"
+	echo -e "Deseas guardar el archivo en el directorio: (si/no)" && pwd
+	read respuesta 
+	if [ $respuesta == "si" ]; then
+	sleep 1
+	echo -e "Como se llamará el archivo? "
+	read nombre_archivo 
+		if [ -f $nombre_archivo ]; then 
+		echo -e "El archivo ya existe. Deseas sobreescribirlo? (si/no)" 
+		read respuesta_de_archivo
+			if [ "$respuesta_de_archivo" == "si" ]; then 
+				echo -e "Creando escaneo UDP y guardando con el nombre: $nombre_archivo"
+				nmap -sS $dire_IP > $nombre_archivo
+				echo -e "Escaneo completado y guardado en: $nombre_archivo"
+			fi 
+			if [ "$respuesta_de_archivo" == "no" ];then 
+				echo -e "Nuevo nombre para el archivo: "
+				echo -e "next"
+			fi
+		fi
+	fi
+	echo -e "Creando un nuevo archivo llamado: $nombre_archivo"
+	sleep 1
+	echo -e "Ejecutando script... "
+	sleep 1
+	nmap -sU $dire_IP > $nombre_archivo
+	if [ -f "$nombre_archivo" ]; then
+		echo -e "Escaneo no completado"
+	else
+		echo -e "Escaneo completado"
+		exit 	
+	fi 
+}
+
+function TCPScan(){
+	clear
+	echo -e "${purpleColour} TCP Scan ${endColour}"
+	echo -e "IP>>"
+	read dire_IP
+	echo -e "Direccion IP establecida: $dire_IP"
+	echo -e "Deseas guardar el archivo en el directorio: (si/no)" && pwd
+	read respuesta 
+	if [ $respuesta == "si" ]; then
+	sleep 1
+	echo -e "Como se llamará el archivo? "
+	read nombre_archivo 
+		if [ -f $nombre_archivo ]; then 
+		echo -e "El archivo ya existe. Deseas sobreescribirlo? (si/no)" 
+		read respuesta_de_archivo
+			if [ "$respuesta_de_archivo" == "si" ]; then 
+				echo -e "Creando escaneo TCP y guardando con el nombre: $nombre_archivo"
+				nmap -sS $dire_IP > $nombre_archivo
+				echo -e "Escaneo completado y guardado en: $nombre_archivo"
+			fi 
+			if [ "$respuesta_de_archivo" == "no" ];then 
+				echo -e "Nuevo nombre para el archivo: "
+				echo -e "next"
+			fi
+		fi
+	fi
+	echo -e "Creando un nuevo archivo llamado: $nombre_archivo"
+	sleep 1
+	echo -e "Ejecutando script... "
+	sleep 1
+	nmap -sU $dire_IP > $nombre_archivo
+	if [ -f "$nombre_archivo" ]; then
+		echo -e "Escaneo no completado"
+	else
+		echo -e "Escaneo completado"
+		exit 	
+	fi 
+
+}
+
 if [ "$(id -u)" -eq "0" ]; then
 	clear
 	echo -e "${redColour}Aviso: Debes estar conectado con el usuario root para un correcto funcionamiento${endColour}"
@@ -145,6 +350,9 @@ if [ "$(id -u)" -eq "0" ]; then
 			8) ScriptsVuln
 			;;
 
+			12) ScriptsNmap
+			;;
+
 			*) exit
 			;;
 		esac
@@ -155,20 +363,3 @@ else
 fi
 
 		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
